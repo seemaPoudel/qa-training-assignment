23d97612dbb06e25e32ef3fbfdb77388acecb148
unknown <simapoudel567@gmail.com>
2025-08-26 21:36:49 +0545

    Remove file

--- sumoftwonumber.py
+++
@@ -1,4 +0,0 @@
-# A program that asks for two numbers and prints their sum.
-userinput1=int(input("Enter first Number="))
-userinput2=int(input("Enter Second Number="))
-print("The Sum is=", userinput1 + userinput2)

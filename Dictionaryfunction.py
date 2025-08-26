e4e0f102352151789ab33244796d8927a0758a85
unknown <simapoudel567@gmail.com>
2025-08-26 21:30:24 +0545

    Delete Dictionaryfunction.py

--- Dictionaryfunction.py
+++
@@ -1,6 +0,0 @@
-# A program that creates a dictionary with keys 'name', 'age', 'city' and prints the values.
-dict1={
-    "Name":"Seema Poudel",
-    "Age": 24,
-    "City":"Anamnagar"}
-print(dict1)
